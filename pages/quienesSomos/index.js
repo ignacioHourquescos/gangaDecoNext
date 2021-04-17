05f4cf6b8d51443325ee17d1f3d8a97133e9c34a
Nico Aru <nico_aru@yahoo.com.ar>
2021-04-17 10:26:28 -0300

    como comprar ok, quienes somos ok, footer falta alinear unos textos y otros detalles, product detail falta acomodar el tema de la imagen. Despues termino todo eso y lo subo.

--- pages/quienesSomos/index.js
+++ pages/quienesSomos/index.js
@@ -4,8 +4,16 @@ const comoComprar = () => {
 
     return (
         <div className={classes.container}>
-            <h1 style={{textAlign:"center", padding:"10rem"}}>Quienes somos</h1>
-
+            <img src="ellas.png"/>
+            <p>
+            Somos <span>Sil</span> y <span>Car</span>, además de socias y amigas también somos hermanas.  
+            </p>
+            <p>
+            Juntas creamos Ganga Deco, que es el resultado de una iniciativa que nació en medio de la cuarentena, un momento que nos permitió reflexionar y descubrir que el hogar es un espacio fundamental en nuestras vidas, donde uno debe estar a gusto y encontrar paz.
+            </p>
+            <p>
+            Empezar fue un gran desafío para nosotras,  estamos felices de que nos ayuden a cumplirlo. Nos esforzamos mucho en la elección de cada producto, buscamos que sean útiles, que los sorprendan y a un precio <span>GANGA</span>. 
+            </p>
         </div>
     )
 }
