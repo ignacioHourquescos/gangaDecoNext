39a19b730414a286f9c3d1badea9d30c3959b9e9
jimancio <ihourquescos@gmail.com>
2021-04-18 16:55:29 -0300

    admin funcionando

--- components/product/ProductList.js
+++ components/product/ProductList.js
@@ -47,5 +47,11 @@ export default function ProductList (props) {
 }
   
 
+
+age.getInitialProps = async (ctx) => {
+    const res = await fetch('https://api.github.com/repos/vercel/next.js')
+    const json = await res.json()
+    return { stars: json.stargazers_count }
+  }
   
 
