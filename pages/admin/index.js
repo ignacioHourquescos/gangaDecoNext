0602153e520fd62e6d2c54d59be3c3329b25d3ed
jimancio <ihourquescos@gmail.com>
2021-04-18 16:57:00 -0300

    admin funcionando

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,5 +1,5 @@
 import FileUpload from '../../firebase/FileUpload'
-import classes from './Index.module.scss'
+import classes from './index.module.scss'
 import { getFirestore } from "../../firebase/index.js";
 import {useState, useEffect} from 'react'
 
