4d6fee62f3627c4e9acd4c74d5c25745f683a203
jimancio <ihourquescos@gmail.com>
2021-04-19 01:25:11 -0300

    cart al 40%

--- components/Checkout/checkout.js
+++ components/Checkout/checkout.js
@@ -14,7 +14,7 @@ const ReactWhatsapp = ({ number,name,message, element, onClick, ...props }) => {
 
     
   if (cart.length>0) {
-    url += `?text=Hola soy ${name}de villa de mayoi %0a Me intersan estos porductos %0a\
+    url += `?text=Hola soy ${name}de Villa de Mayo %0a Me intersan estos productos %0a\
     ${cart[0].quantity} x ${cart[0].id} x ${cart[0].desc} %0a\
     ${cart[1].quantity} x ${cart[1].id} x ${cart[1].desc} %0a\
     ${cart[2].quantity} x ${cart[2].id} x ${cart[2].desc} %0a`;
